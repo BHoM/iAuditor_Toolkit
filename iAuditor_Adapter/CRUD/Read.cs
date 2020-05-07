b28dfc67352771f1e3cf67b9b4eb78afa1d88f7f
Erik Narhi <Erik.Narhi@burohappold.com>
2020-05-06 17:04:49 -0700

    Initial prototype WIP

---
+++ iAuditor_Adapter/CRUD/Read.cs
@@ -0,0 +1,233 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using BH.Adapter.iAuditor;
+using BH.oM.Base;
+using BH.oM.Adapter;
+using BH.oM.Adapter.iAuditor;
+using BH.oM.HTTP;
+using BH.Engine.HTTP;
+using BH.Engine.iAuditor;
+using BH.Adapter;
+using BH.Engine.Serialiser;
+using BH.Engine.Reflection;
+
+namespace BH.Adapter.iAuditor
+{
+    public partial class iAuditorAdapter : BHoMAdapter
+    {
+        /***************************************************/
+        /**** Adapter overload method                   ****/
+        /***************************************************/
+        //protected override IEnumerable<IBHoMObject> IRead(Type type, IList ids, ActionConfig actionConfig = null)
+        //{
+        //    dynamic elems = null;
+        //    iAuditorConfig config = null;
+
+        //    if (actionConfig is iAuditorConfig)
+        //        config = actionConfig as iAuditorConfig;
+
+        //    //Choose what to pull out depending on the type.
+        //    if (type == typeof(EnvironmentalProductDeclaration))
+        //        elems = ReadEnvironmentalProductDeclaration(ids as dynamic, config);
+        //    else if (type == typeof(SectorEnvironmentalProductDeclaration))
+        //        elems = ReadSectorEnvironmentalProductDeclaration(ids as dynamic, config);
+
+        //    return elems;
+        //}
+
+        ///***************************************************/
+        ///**** Private specific read methods             ****/
+        ///***************************************************/
+        //private List<EnvironmentalProductDeclaration> ReadEnvironmentalProductDeclaration(List<string> ids = null, iAuditorConfig config = null)
+        //{
+        //    //Add parameters per config
+        //    CustomObject requestParams = new CustomObject();
+        //    int count = 0;
+        //    string name = null;
+        //    string plantName = null;
+        //    string id = null;
+
+        //    if (config != null)
+        //    {
+        //        id = config.Id;
+        //        count = config.Count;
+        //        name = config.NameLike;
+        //        plantName = config.PlantName;
+                
+        //        //id returns specific id and therefore supersedes other parameters
+        //        if (id == null)
+        //        {
+        //            if (count != 0)
+        //            {
+        //                requestParams.CustomData.Add("page_size", count);
+        //            }
+        //            if (name != null)
+        //            {
+        //                requestParams.CustomData.Add("name__like", name);
+        //            }
+        //            if (plantName != null)
+        //            {
+        //                requestParams.CustomData.Add("plant__name__like", plantName);
+        //            }
+        //        }
+        //    }
+
+        //    //Create GET Request
+        //    GetRequest epdGetRequest;
+        //    if (id == null)
+        //    { epdGetRequest = BH.Engine.CarbonQueryDatabase.Create.CarbonQueryDatabaseRequest("epds", m_bearerToken, requestParams); }
+        //    else
+        //    { epdGetRequest = BH.Engine.CarbonQueryDatabase.Create.CarbonQueryDatabaseRequest("epds/" + id, m_bearerToken); }
+
+        //    string reqString = epdGetRequest.ToUrlString();
+        //    string response = BH.Engine.HTTP.Compute.MakeRequest(epdGetRequest);
+        //    List<object> responseObjs = null;
+        //    if (response == null)
+        //    {
+        //        BH.Engine.Reflection.Compute.RecordWarning("No response received, check bearer token and connection.");
+        //        return null;
+        //    }
+
+        //    //Check if the response is a valid json
+        //    else if (response.StartsWith("{"))
+        //    {
+        //        response = "[" + response + "]";
+        //        responseObjs = new List<object>() { Engine.Serialiser.Convert.FromJson(response) };
+        //    }
+        //    else if (response.StartsWith("["))
+        //    {
+        //        responseObjs = new List<object>() { Engine.Serialiser.Convert.FromJson(response) };
+        //    }
+
+        //    else
+        //    {
+        //        BH.Engine.Reflection.Compute.RecordWarning("Response is not a valid JSON. How'd that happen?");
+        //        return null;
+        //    }
+
+        //    //Convert nested customObject from serialization to list of epdData objects
+        //    List<EnvironmentalProductDeclaration> epdDataFromRequest = new List<EnvironmentalProductDeclaration>();
+
+        //    object epdObjects = Engine.Reflection.Query.PropertyValue(responseObjs[0], "Objects");
+
+        //    IEnumerable objList = epdObjects as IEnumerable;
+        //    if (objList != null)
+        //    {
+        //        foreach (CustomObject co in objList)
+        //        {
+        //            EnvironmentalProductDeclaration epdData = Adapter.CarbonQueryDatabase.Convert.ToEnvironmentalProductDeclaration(co);
+        //            epdDataFromRequest.Add(epdData);
+        //        }
+        //    }
+
+        //    return epdDataFromRequest;
+        //}
+
+        ///***************************************************/
+
+        //private List<SectorEnvironmentalProductDeclaration> ReadSectorEnvironmentalProductDeclaration(List<string> ids = null, iAuditorConfig config = null)
+        //{
+        //    //Add parameters per config
+        //    CustomObject requestParams = new CustomObject();
+        //    int count = 0;
+        //    string name = null;
+        //    string id = null;
+
+        //    if (config != null)
+        //    {
+        //        id = config.Id;
+        //        count = config.Count;
+        //        name = config.NameLike;
+
+        //        //id returns specific id and therefore supersedes other parameters
+        //        if (id == null)
+        //        {
+        //            if (count != 0)
+        //            {
+        //                requestParams.CustomData.Add("page_size", count);
+        //            }
+        //            if (name != null)
+        //            {
+        //                requestParams.CustomData.Add("name__like", name);
+        //            }
+        //        }
+        //    }
+
+        //    //Create GET Request
+        //    GetRequest epdGetRequest;
+        //    if (id == null)
+        //    { epdGetRequest = BH.Engine.CarbonQueryDatabase.Create.CarbonQueryDatabaseRequest("industry_epds", m_bearerToken, requestParams); }
+        //    else
+        //    { epdGetRequest = BH.Engine.CarbonQueryDatabase.Create.CarbonQueryDatabaseRequest("industry_epds" + id, m_bearerToken); }
+
+        //    string response = BH.Engine.HTTP.Compute.MakeRequest(epdGetRequest);
+        //    List<object> responseObjs = null;
+        //    if (response == null)
+        //    {
+        //        BH.Engine.Reflection.Compute.RecordWarning("No response received, check bearer token and connection.");
+        //        return null;
+        //    }
+
+        //    //Check if the response is a valid json
+        //    else if (response.StartsWith("{"))
+        //    {
+        //        response = "[" + response + "]";
+        //        responseObjs = new List<object>() { Engine.Serialiser.Convert.FromJson(response) };
+        //    }
+        //    else if (response.StartsWith("["))
+        //    {
+        //        responseObjs = new List<object>() { Engine.Serialiser.Convert.FromJson(response) };
+        //    }
+
+        //    else
+        //    {
+        //        BH.Engine.Reflection.Compute.RecordWarning("Response is not a valid JSON. How'd that happen?");
+        //        return null;
+        //    }
+
+        //    //Convert nested customObject from serialization to list of epdData objects
+        //    List<SectorEnvironmentalProductDeclaration> epdDataFromRequest = new List<SectorEnvironmentalProductDeclaration>();
+
+        //    object epdObjects = Engine.Reflection.Query.PropertyValue(responseObjs[0], "Objects");
+        //    IEnumerable objList = epdObjects as IEnumerable;
+        //    if (objList != null)
+        //    {
+        //        foreach (CustomObject co in objList)
+        //        {
+        //            SectorEnvironmentalProductDeclaration epdData = Adapter.CarbonQueryDatabase.Convert.ToSectorEnvironmentalProductDeclaration(co);
+        //            epdDataFromRequest.Add(epdData);
+        //        }
+        //    }
+
+        //    return epdDataFromRequest;
+        //}
+
+        /***************************************************/
+
+    }
+
+}
---
+++ iAuditor_oM/Adapter/iAuditorConfig.cs
@@ -0,0 +1,52 @@
+/*
+ * This file is part of the Buildings and Habitats object Model (BHoM)
+ * Copyright (c) 2015 - 2020, the respective contributors. All rights reserved.
+ *
+ * Each contributor holds copyright over their respective contributions.
+ * The project versioning (Git) records all such contribution source information.
+ *                                           
+ *                                                                              
+ * The BHoM is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3.0 of the License, or          
+ * (at your option) any later version.                                          
+ *                                                                              
+ * The BHoM is distributed in the hope that it will be useful,              
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                 
+ * GNU Lesser General Public License for more details.                          
+ *                                                                            
+ * You should have received a copy of the GNU Lesser General Public License     
+ * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
+ */
+
+using BH.oM.Adapter;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace BH.oM.Adapter.iAuditor
+{
+    [Description("This Config can be specified in the `ActionConfig` input of any Adapter Action (e.g. Push).")]
+    // Note: this will get passed within any CRUD method (see their signature). 
+    // In order to access its properties, you will need to cast it to `CarbonQueryDatabaseActionConfig`.
+    public class iAuditorConfig : ActionConfig
+    {
+        /***************************************************/
+        /**** Public Properties                         ****/
+        /***************************************************/
+
+        [Description("Specifies ID to search and return objects for in CarbonQueryDatabase. If this is specified it supersedes other input parameters.")]
+        public virtual string Id { get; set; } = null;
+
+        [Description("Sets maximum amount of items to return from CarbonQueryDatabase")]
+        public virtual int Count { get; set; } = 0;
+
+        [Description("Specifies string to search and return objects for in CarbonQueryDatabase, ie RedBuilt RedLam LVL")]
+        public virtual string NameLike { get; set; } = null;
+
+        [Description("Specifies plant name to search and return objects for in CarbonQueryDatabase, ie Dupont")]
+        public virtual string PlantName { get; set; } = null;
+
+        /***************************************************/
+    }
+}
